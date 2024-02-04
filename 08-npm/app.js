31540820cdf60cccd656b4625c2df5ed90efc6bc
unknown <sigitnj89@gmail.com>
2024-02-04 22:52:32 +0700

    Using Package NPM

--- 08-npm/app.js
+++ 08-npm/app.js
@@ -1,1 +1,8 @@
-console.log('Running');
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('Sigit@gmail.com'));
+// console.log(validator.isMobilePhone('082123123', 'id-ID'));
+// console.log(validator.isNumeric('082123123'));
+
+console.log(chalk.blue('Sigit'));
