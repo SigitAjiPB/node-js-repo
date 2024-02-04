d3862fa5a0a2e9e4c287002f5a4bab1741992b2c
unknown <sigitnj89@gmail.com>
2024-02-04 11:39:02 +0700

    Make up contact data using syncronous module

--- 07-node-core-module/app.js
+++ 07-node-core-module/app.js
@@ -28,10 +28,43 @@ const fs = require('fs');
 
 
 //================== membaca isi file seara asyncronous =================
-fs.readFile('data/test.txt','utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
+// fs.readFile('data/test.txt','utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+
+
+
+// =============== read line =================
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+rl.question('Masukan nama anda: ', (nama) => {
+
+  rl.question('Masukan No Telpon: ', (noTelp) => {
+    const contact = {
+      nama, noTelp
+    }
+    const file = fs.readFileSync('data/contacts.JSON', 'utf8');
+    const contacts = JSON.parse(file);
+  
+    contacts.push(contact)
+    
+    fs.writeFileSync('data/contacts.JSON', JSON.stringify(contacts));
+    console.log("Data Sudah terinput")
+
+    rl.close();
+
+
+  });
+
+});
+
+
 
 
 
